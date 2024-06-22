0b0a2c13cb4f14d0e0a054fe0d05b8793ccdf48c
Askarbekov Aktan <akn392204@gmail.com>
2024-06-22 16:19:34 +0600

    Add dataseed

--- Models/WalletContext.cs
+++ Models/WalletContext.cs
@@ -10,6 +10,9 @@ public class WalletContext : IdentityDbContext<User, IdentityRole<int>, int>
     
     public DbSet<User> Users { get; set; }
     public DbSet<Transaction> Transactions { get; set; }
+    public DbSet<ServiceProvider> ServiceProviders { get; set; }
+    public DbSet<ServiceUser> ServiceUsers { get; set; }
+
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
@@ -26,5 +29,15 @@ public class WalletContext : IdentityDbContext<User, IdentityRole<int>, int>
             .WithMany(u => u.TransactionsTo)
             .HasForeignKey(t => t.ToUserId)
             .OnDelete(DeleteBehavior.Restrict);
+        
+        modelBuilder.Entity<ServiceProvider>().HasData(
+            new ServiceProvider { Id = 1, Name = "Operator A" },
+            new ServiceProvider { Id = 2, Name = "Internet Provider B" }
+        );
+
+        modelBuilder.Entity<ServiceUser>().HasData(
+            new ServiceUser { Id = 1, ServiceProviderId = 1, Identifier = "1234567890", Balance = 100 },
+            new ServiceUser { Id = 2, ServiceProviderId = 2, Identifier = "0987654321", Balance = 150 }
+        );
     }
 }
