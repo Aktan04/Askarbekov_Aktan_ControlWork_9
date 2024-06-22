df4848fbe59720f6865a54fce2ea49013796a3da
Askarbekov Aktan <akn392204@gmail.com>
2024-06-22 17:02:53 +0600

    Add migrations

---
+++ Migrations/20240622103022_ChangeTypeOfPersonalAccount.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Cw9.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeTypeOfPersonalAccount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PersonalAccount",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PersonalAccount",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
