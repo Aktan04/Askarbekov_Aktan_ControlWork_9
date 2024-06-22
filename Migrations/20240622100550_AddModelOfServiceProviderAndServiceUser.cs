798a56100c829e3bd573f6922e40381d41b70a83
Askarbekov Aktan <akn392204@gmail.com>
2024-06-22 16:18:41 +0600

    Add new migrations

---
+++ Migrations/20240622100550_AddModelOfServiceProviderAndServiceUser.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Cw9.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddModelOfServiceProviderAndServiceUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ToUserId",
+                table: "Transactions",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Balance",
+                table: "AspNetUsers",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.CreateTable(
+                name: "ServiceProviders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiceProviders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServiceUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ServiceProviderId = table.Column<int>(type: "integer", nullable: false),
+                    Identifier = table.Column<string>(type: "text", nullable: false),
+                    Balance = table.Column<decimal>(type: "numeric", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiceUsers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ServiceUsers_ServiceProviders_ServiceProviderId",
+                        column: x => x.ServiceProviderId,
+                        principalTable: "ServiceProviders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "ServiceProviders",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Operator A" },
+                    { 2, "Internet Provider B" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ServiceUsers",
+                columns: new[] { "Id", "Balance", "Identifier", "ServiceProviderId" },
+                values: new object[,]
+                {
+                    { 1, 100m, "1234567890", 1 },
+                    { 2, 150m, "0987654321", 2 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServiceUsers_ServiceProviderId",
+                table: "ServiceUsers",
+                column: "ServiceProviderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ServiceUsers");
+
+            migrationBuilder.DropTable(
+                name: "ServiceProviders");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ToUserId",
+                table: "Transactions",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Balance",
+                table: "AspNetUsers",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+        }
+    }
+}
